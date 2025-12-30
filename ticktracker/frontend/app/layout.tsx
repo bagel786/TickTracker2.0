f77c1bdf110457781c2bc429dbd6a58e2edcf5f7
bagel786 <safbaig5@gmail.com>
2025-12-29 18:03:35 -0600

    Add Google Analytics scripts to RootLayout

--- ticktracker/frontend/app/layout.tsx
+++ ticktracker/frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -17,6 +18,19 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-VBWWR89NJW"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-VBWWR89NJW');
+                    `}
+                </Script>
                 <main className="min-h-screen flex flex-col">
                     {children}
                 </main>
